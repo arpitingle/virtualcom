7e77085fa0e64d4ca9acfa003f1a3344dfb9bedd
Arpit Ingle <arpitingle1@gmail.com>
2023-08-18 10:32:49 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -6,6 +6,6 @@ baud_rate = 9600
 
 with serial.Serial(sender_port, baud_rate, timeout=1) as ser:
     while True:
-        ser.write(b'Hello from sender!')
+        ser.write(b'Hello from sender!\n')
         ser.flush()
         time.sleep(2)  # Send data every two seconds
